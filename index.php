77dea51c7f3bb7a2a2a80e58b2838a5238a87922
cluvfccii <cicinrplah@gmail.com>
2024-08-13 15:00:14 +0700

    menambah login

--- index.php
+++ index.php
@@ -1,114 +1,210 @@
+<?php
+session_start();
+
+
+?>
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Mobil</title>
-  <style>
-    * {
-      box-sizing: border-box;
-    }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>rental mobil</title>
 
-    body {
-      font-family: Arial, Helvetica, sans-serif;
-    }
+  <!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet">
 
-    .column {
-      float: left;
-      width: 25%;
-      padding: 0 10px;
-      margin-top: 20px;
-    }
+<!-- Feather icons -->
+<script src="https://unpkg.com/feather-icons"></script>
 
-    .btn {
-      padding: 10px 15px;
-      background-color: #b6895b;
-      color: white;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      font-size: 16px;
-      transition: background-color 0.3s;
-      text-decoration: none;
-    }
-
-    .row {
-      margin: 0 -5px;
-    }
-
-    .row:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-
-    @media screen and (max-width: 600px) {
-      .column {
-        width: 100%;
-        display: block;
-        margin-bottom: 20px;
-      }
-    }
-
-    .card {
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-      padding: 16px;
-      text-align: center;
-      background-color: #f1f1f1;
-    }
-
-    .card img {
-      width: 100%;
-      height: auto;
-    }
-
-    .btn-add {
-      background-color: #4CAF50;
-      color: white;
-      padding: 10px 15px;
-      text-decoration: none;
-      border-radius: 4px;
-      display: inline-block;
-      margin-bottom: 20px;
-    }
-  </style>
+<!-- my style -->
+<link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
-  <div class="container">
-    <a class="btn-add" href="tambah.php">Tambah Mobil</a>
+   <!-- Navbar start -->
+   <nav class="navbar">
+    <a href="#" class="navbar-logo">rental mobil<span>_Cici</span>.</a>
+    
+          <div class="navbar-nav">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#menu">Our Time</a>
+      <a href="datamobil.html">Data mobil</a></a>
+      <a href="datakostumer.html">Data kostumer</a>
+      <a href="#contact">Contact</a>
+      <?php
+      
+      if (isset($_SESSION['username'])) {
+         
+          echo '<a href="logout.php"><i data-feather="log-out"></i> Logout</a>';
+      } else {
+          
+          echo '<a href="login.php"><i data-feather="log-in"></i> Login</a>';
+      }
+      ?>
+    </div>
+    
+    <div class="navbar-extra">
+<a href="#" id="search"><i data-feather="search"></i></a>
+<a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
+<a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
   </div>
-  <div class="row">
-    <?php
-    include 'koneksi.php';
+  
+      </nav>
+  <!-- Navbar end -->
+  
+  <!-- Hero section start -->
+  <section class="hero" id="home">
+    <main class="content">
+      <h1>Welcome to rental<span>mobil</span>
+      </h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium, enim.</p>
+      <a href="#" class="cta">sewa Sekarang</a>
+    </main>
+  </section>
+      <!-- Hero section end -->
 
-    $sql = "SELECT m.id_mobil, m.nama_mobil, m.tahun, m.warna,j.nama_kategori, m.harga
-            FROM mobil m 
-            JOIN kategori j ON m.id_kategori = j.id_kategori";
-    $result = $conn->query($sql);
+       <!-- about section start -->
+       <section id="about" class="about">
+        <h2><span>about</span> us</h2>
+        <div class="row">
+        <div class="about-img">
+        <img src="./img/a4.jpg" width=300 height=400>
+        </div>
+        <div class="content">
+          <h3>Mengapa memilih kami?</h3>
+          <p>lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium, enim.</p>
+          <p>lorem ipsum dolor sit amet consectetur adipisicing elit.Accusantium, enim.</p>
+        </div>
+      </section>
+      <!-- about section end -->
+       
+      <!-- menu section star -->
+      <section id="menu" class="menu">
+        <h2><span>Our</span> time</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat ex necessitatibus.</p>
+        <br>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="img/d2.jpg" alt="Bmw M4" style="width:100%">
+            <div class="container">
+              <h2>Jane Doe</h2>
+              <p class="title">CEO & Founder</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>jane@example.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      
+        <div class="column">
+          <div class="card">
+            <img src="img/a1.jpg" alt="Mike" style="width:100%">
+            <div class="container">
+              <h2>Mike Ross</h2>
+              <p class="title">Art Director</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>mike@example.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      
+        <div class="column">
+          <div class="card">
+            <img src="img/a2.jpg" alt="John" style="width:100%">
+            <div class="container">
+              <h2>John Doe</h2>
+              <p class="title">Designer</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>john@example.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- menu section end -->
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo '<div class="column">';
-            echo '<div class="card">';
-            echo '<img src="#" alt="' . $row["nama_mobil"] . '">';
-            echo '<div class="container">';
-            echo '<h4><b>' . $row["nama_mobil"] . '</b></h4>';
-            echo '<p>tahun: ' . $row["tahun"] . '</p>';
-            echo '<p>warna: ' . $row["warna"] . '</p>';
-            echo '<p>Kategori: ' . $row["nama_kategori"] . '</p>';
-            echo '<p>harga: ' . $row["harga"] . '</p>';
-            echo '<a class="btn" href="datamobil.html?id=' . $row["id_mobil"] . '">Detail Mobil</a>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
-        }
-    } else {
-        echo "Tidak ada data yang ditemukan.";
-    }
-    $conn->close();
-    ?>
-  </div>
+      <!-- contact us start -->
+      <section id="contact" class="contact">
+        <h2><span>contact</span> us</h2>
+          <tr>
+            <div class="contact_us_6">
+              <div class="responsive-container-block container">
+                <form class="form-box">
+                  <div class="container-block form-wrapper">
+                    <div class="mob-text">
+                      <p class="text-blk contactus-head">
+                        Get in Touch
+                      </p>
+                      <p class="text-blk contactus-subhead">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis diam lectus sapien.
+                      </p>
+                    </div>
+                    <div class="responsive-container-block" id="i2cbk">
+                      <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i10mt-3">
+                        <p class="text-blk input-title">
+                          FIRST NAME
+                        </p>
+                        <input class="input" id="ijowk-3" name="FirstName" placeholder="Please enter first name...">
+                      </div>
+                      <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ip1yp">
+                        <p class="text-blk input-title">
+                          EMAIL
+                        </p>
+                        <input class="input" id="ipmgh-3" name="Email" placeholder="Please enter email...">
+                      </div>
+                      <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ih9wi">
+                        <p class="text-blk input-title">
+                          PHONE NUMBER
+                        </p>
+                        <input class="input" id="imgis-3" name="PhoneNumber" placeholder="Please enter phone number...">
+                      </div>
+                      <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i634i-3">
+                        <p class="text-blk input-title">
+                          WHAT DO YOU HAVE IN MIND ?
+                        </p>
+                        <textarea class="textinput" id="i5vyy-3" placeholder="Please enter query..."></textarea>
+                      </div>
+                    </div>
+                    <button class="submit-btn" id="w-c-s-bgc_p-1-dm-id-2">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+                <div class="responsive-cell-block wk-desk-7 wk-ipadp-12 wk-tab-12 wk-mobile-12" id="i772w">
+                  <div class="map-part">
+                    <p class="text-blk map-contactus-head" id="w-c-s-fc_p-1-dm-id">
+                      Reach us at
+                    </p>
+                    <p class="text-blk map-contactus-subhead">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis diam lectus sapien.
+                    </p>
+                    <div class="social-media-links mob">
+                      <a class="social-icon-link" href="#" id="ix94i-2-2">
+                        <img class="link-img image-block" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-twitter.png">
+                      </a>
+                      <a class="social-icon-link" href="#" id="itixd">
+                        <img class="link-img image-block" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-facebook.png">
+                      </a>
+                      <a class="social-icon-link" href="#" id="izxvt">
+                        <img class="link-img image-block" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-google.png">
+                      </a>
+                      <a class="social-icon-link" href="#" id="izldf-2-2">
+                        <img class="link-img image-block" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-instagram.png">
+                      </a>
+                    </div>
+                    <div class="map-box container-block">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </tr>
+        </section>
+        <!-- contact us end -->
 </body>
-
 </html>
---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+include 'koneksi.php';
+session_start();
+ 
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
+}
+ 
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = hash('sha256', $_POST['password']); // Hash the input password using SHA-256
+ 
+    $sql = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+ 
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username'] = $row['username'];
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "<script>alert('Email atau password Anda salah. Silakan coba lagi!')</script>";
+    }
+}
+?>
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Login form in HTML and CSS</title>
+    <link rel="stylesheet" href="style3.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="wrapper">
+        <form action="" method="POST" class="login-email">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="email" name="email" required />
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="Password" placeholder="password" name="password" required />
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox" /> Remember me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button name="submit" class="btn">Login</a></button>
+            <div class="register-link">
+                <p>Create on account</a></p>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
